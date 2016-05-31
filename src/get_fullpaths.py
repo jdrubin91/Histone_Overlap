18de416eb342b4e3dc875fadc8e6a57f9bd6cc85
jdrubin91 <jd.rubin91@gmail.com>
2016-05-31 10:59:24 -0600

    updates

--- src/get_fullpaths.py
+++ src/get_fullpaths.py
@@ -7,7 +7,7 @@ def run(TFdir,Histonedir):
     with open(TFdir + 'metadata.tsv') as F:
         for line in F:
             line = line.strip().split()
-            if line[1] is 'bed' and line[3] is 'peaks' and 'control' not in line[-3]:
+            if line[1] == 'bed' and line[3] == 'peaks' and 'control' not in line[-3]:
                 TFdict[line[-3]] = TFdir + line[0] + '.sorted.cut'
                 
     Histonedict = dict()
